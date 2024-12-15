208dd9e8d83c963e580d60cb8b9839a915b3fc38
PedroHFG <pedrohfidg@gmail.com>
2024-12-14 22:02:42 -0300

    Created Task entity

---
+++ src/main/java/com/dev/taskmanager/entities/Task.java
@@ -0,0 +1,79 @@
+package com.dev.taskmanager.entities;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class Task {
+
+    private Long id;
+    private String title;
+    private String description;
+    private TaskStatus status;
+    private LocalDate dueDate;
+
+    public Task() {
+
+    }
+
+    public Task(Long id, String title, String description, TaskStatus status, LocalDate dueDate) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.status = status;
+        this.dueDate = dueDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public TaskStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(TaskStatus status) {
+        this.status = status;
+    }
+
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Task task = (Task) o;
+        return Objects.equals(id, task.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
